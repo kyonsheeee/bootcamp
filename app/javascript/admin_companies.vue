1d6d1eaa8687b514afa19cbad52d12e7c0874ae4
Miki Watanabe <watami03@gmail.com>
2022-04-03 14:44:18 +0200

    企業一覧から削除ボタンを削除

--- app/javascript/admin_companies.vue
+++ app/javascript/admin_companies.vue
@@ -45,11 +45,6 @@
                   :href='`/admin/companies/${company.id}/edit`'
                 )
                   i.fas.fa-pen
-              li
-                a.a-button.is-sm.is-danger.is-icon.js-delete(
-                  @click='destroy(company)'
-                )
-                  i.fas.fa-trash-alt
   nav.pagination(v-if='totalPages > 1')
     pager(v-bind='pagerProps')
 </template>
