4ab87074fbff07a5cc72abbaf93d15514da4e607
eatplaynap <eatplaynap329@gmail.com>
2022-04-08 21:42:48 +0900

    提出物一覧画面の担当ボタンが正しく機能するよう修正

--- app/javascript/product.vue
+++ app/javascript/product.vue
@@ -118,7 +118,8 @@
         :checkerName='product.checker_name',
         :checkerAvatar='product.checker_avatar',
         :currentUserId='currentUserId',
-        :productId='product.id'
+        :productId='product.id',
+        :parentComponent='"product"'
       )
 </template>
 <script>
